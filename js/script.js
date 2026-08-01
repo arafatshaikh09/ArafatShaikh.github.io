deaf74c38123498dfa0f5a56a31ebc79d1d1aaf1
Arafat <arafath9.shaikh@gmail.com>
2026-08-01 23:49:10 +0530

    more additions

--- js/script.js
+++ js/script.js
@@ -67,3 +67,50 @@ setTimeout(type,isDeleting?60:120);
 }
 
 type();
+// ================= STATS COUNTER =================
+
+const counters = document.querySelectorAll(".counter");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            const counter = entry.target;
+
+            const target = +counter.dataset.target;
+
+            let count = 0;
+
+            const update = () => {
+
+                const increment = target / 120;
+
+                if(count < target){
+
+                    count += increment;
+
+                    counter.innerText = Math.ceil(count);
+
+                    requestAnimationFrame(update);
+
+                }else{
+
+                    counter.innerText = target + "+";
+
+                }
+
+            };
+
+            update();
+
+            observer.unobserve(counter);
+
+        }
+
+    });
+
+});
+
+counters.forEach(counter => observer.observe(counter));
